ab332e68f77ecdf782b7986044d720203d1ed7e8
ericanthony1286 <nnquangnd@gmail.com>
2023-05-09 10:25:36 +0700

    18

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ import { APP_ID } from "./api/hello";
 
 export default function App({ Component, pageProps }: AppProps) {
   const id = process.env.NEXT_PUBLIC_APP_ID;
-  console.log(id);
+  console.log("APP_ID: ", id);
   useEffect(() => {
     window.fbAsyncInit = function () {
       window.FB.init({
